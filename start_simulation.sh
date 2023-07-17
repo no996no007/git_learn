a6361c0aa8002560831e601caecd219beaa192df
no996no007 <799459870@qq.com>
2023-07-17 22:19:37 +0800

    nothing

--- start_simulation.sh
+++ start_simulation.sh
@@ -21,19 +21,16 @@ sleep 1.5s
 # gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;roslaunch apriltag_ros continuous_detection.launch;exec bash;"
 # sleep 1.5s
 
+#启动keyboard控制器
+gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;rosrun uav_simulation keyboard_control.py;exec bash;"
+sleep 1.5s
+
 gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;roslaunch apriltag_ros start.launch;exec bash;"
 sleep 1.5s
 
-
-# #启动keyboard控制器
-# gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;rosrun uav_simulation keyboard_control.py;exec bash;"
-# sleep 1.5s
-
-#启动keyboard控制器
 # gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;rosrun uav_simulation detector;exec bash;"
 # sleep 1.5s
 
-#启动我们的控制器
 # gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;rosrun uav_simulation controller.py;exec bash;"
 # sleep 1.5s
 
